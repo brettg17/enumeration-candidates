36b2dff03d4bbadfa9b2fceba990398bfa3146bf
brettg17 <brettgelinas7@gmail.com>
2024-06-21 14:17:11 -0600

    completed assignment

--- filters.rb
+++ filters.rb
@@ -1,16 +1,25 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
+ require_relative 'candidates'
 
 def find(id)
-    # Your code Here
+    @candidates.find{ |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    pp candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates.select do |candidate|
+      experienced?(candidate) && 
+      candidate[:github_points] >= 100 &&
+      (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+      (Date.today - candidate[:date_applied]).to_i <= 15 &&
+      candidate[:age] > 17
+    end
   end
   
-  # More methods will go below
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
--- main.rb
+++ main.rb
@@ -3,7 +3,29 @@
 require 'pp'
 require './candidates'
 require './filters'
+require 'active_support'
+require 'active_support/core_ext'
+require './filters'
 
 ## Your test code can go here
 
 pp @candidates
+
+candidate_id = 5
+found_candidate = find(candidate_id)
+
+puts "Candidate with ID #{candidate_id}"
+pp found_candidate
+
+experienced_candidates = @candidates.select { |candidate| experienced?(candidate) }
+
+qualified_candidates_list = qualified_candidates(@candidates)
+
+puts "\nQualified Candidates:"
+pp qualified_candidates_list
+
+# Order candidates by qualifications
+ordered_candidates = ordered_by_qualifications(@candidates)
+
+puts "\nCandidates Ordered by Qualifications:"
+pp ordered_candidates
